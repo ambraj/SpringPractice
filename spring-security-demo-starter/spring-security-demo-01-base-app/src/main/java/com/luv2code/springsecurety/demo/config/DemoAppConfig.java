afe6a5c6b29e2b14509c94d2fabdf92462e9bde0
Ambuj Rajput <ambraj@gmail.com>
2018-03-03 01:34:35 +0530

    spring security

---
+++ spring-security-demo-starter/spring-security-demo-01-base-app/src/main/java/com/luv2code/springsecurety/demo/config/DemoAppConfig.java
@@ -0,0 +1,11 @@
+package com.luv2code.springsecurety.demo.config;
+
+import org.springframework.cotext.annotation.ComponentScan;
+import org.springframework.cotext.annotation.Configuration;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class DemoAppConfig {
+
+}
